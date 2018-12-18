0a2fc35097cf5d0f79fce2225981d517b97ca392
ilayFriedman <ilayfri@bgu.ac.il>
2018-12-18 17:10:15 +0200

    final part a

--- Searcher.py
+++ Searcher.py
@@ -1,8 +1,28 @@
+import ujson
+
 from Parse import Parse
+from Ranker import Ranker
 
 
 class Searcher:
-    def __init__(self,query,stopWord):
-        self.originalQuery = query
-        self.parseQuery = Parse(stopWord).parseText(query)
+    def __init__(self,stopWords,basePath,docPath,doStem):
+        if(doStem):
+            with open(basePath + "/S_baseDict.ujson", "r+" ) as file:
+                baseDic = ujson.load(file)
+                file.close()
+            with open(docPath + "/S_fileIndex.ujson", "r+" ) as file1:
+                fileIndex = ujson.load(file1)
+                file1.close()
+        else:
+            with open(basePath + "/baseDict.ujson", "r+" ) as file:
+                baseDic = ujson.load(file)
+                file.close()
+            with open(docPath + "/fileIndex.ujson", "r+" ) as file1:
+                fileIndex = ujson.load(file1)
+                file1.close()
+        self.ranker = Ranker(baseDic,fileIndex)
+        self.stopWords= stopWords
+
+    def singleQueryCalc(self,query):
+        parseQuery = Parse(self.stopWords).parseText(query)
 
